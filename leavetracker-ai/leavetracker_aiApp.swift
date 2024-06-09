44c984e6bbb09412ac42d0972642ad2d30744e55
esme-putt <esme.putt@gmail.com>
2024-06-09 13:06:00 +1200

    Initial Commit

---
+++ leavetracker-ai/leavetracker_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  leavetracker_aiApp.swift
+//  leavetracker-ai
+//
+//  Created by Esme Putt on 9/06/24.
+//
+
+import SwiftUI
+
+@main
+struct leavetracker_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

19d67c25be22061248320ef66567b4799938f4b2
esme-putt <esme.putt@gmail.com>
2024-06-18 20:08:02 +1200

    Make changes to leave info form

--- leavetracker-ai/LeaveInfoForm.swift
+++ leavetracker-ai/LeaveInfoForm.swift
@@ -6,20 +6,63 @@ struct LeaveInfoForm: View {
     var body: some View {
         NavigationView {
             Form {
-                TextField("Weeks of Annual Leave", text: $leaveInfo.annualLeaveWeeks)
-                    .keyboardType(.numberPad)
-                    .padding()
-                
-                TextField("Hours Worked Per Week", text: $leaveInfo.hoursPerWeek)
-                    .keyboardType(.numberPad)
-                    .padding()
-                
+                HStack {
+                    Text("Weeks of annual leave entitled")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    TextField("Weeks", text: $leaveInfo.annualLeaveWeeks)
+                        .keyboardType(.numberPad)
+                        .padding()
+                }
+                HStack {
+                    Text("Hours worked per week")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    TextField("Hours", text: $leaveInfo.hoursPerWeek)
+                        .keyboardType(.numberPad)
+                        .padding()
+                }
+                VStack {
+                    Text("Regular working days")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical)
+                    HStack {
+                        Button("Mon") { }
+                            .frame(maxWidth: 60, maxHeight: 40)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(4)
+                        Button("Tues") { }
+                            .frame(maxWidth: 60, maxHeight: 40)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(4)
+                        Button("Wed") { }
+                            .frame(maxWidth: 60, maxHeight: 40)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(4)
+                        Button("Thur") { }
+                            .frame(maxWidth: 60, maxHeight: 40)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(4)
+                        Button("Fri") { }
+                            .frame(maxWidth: 60, maxHeight: 40)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(4)
+                    }
+                }
+                            
                 Button("Save") {
                 }
                 .frame(maxWidth: .infinity, maxHeight: 50)
                 .background(Color.blue)
                 .foregroundColor(.white)
                 .cornerRadius(10)
+                .padding()
             }
             .navigationBarTitle("Leave information", displayMode: .inline)
         }
